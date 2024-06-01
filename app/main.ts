0ee89f1ceee1ea4c6c0f5afce9bdba40257418b3
Loi Nguyen <nguyenthanhloi.theone@gmail.com>
2024-06-01 12:40:08 +0700

    stage 3 - Extract URL path

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,32 @@
 import * as net from 'net';
 
+const METHOD_INDEX = 0
+const PATH_REQUEST_INDEX = 1
+const HTTP_VERSION_INDEX = 2
+
+
 const server = net.createServer((socket) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n')
-    socket.end();
+    socket.on('data', (data) => {
+        const dataString = data.toString()
+        const dataArray = dataString.split('\r')
+
+        const requestInfo = dataArray[0].split(' ')
+
+        const method = requestInfo[METHOD_INDEX]
+        const pathRequest = requestInfo[PATH_REQUEST_INDEX]
+        const httpVersion = requestInfo[HTTP_VERSION_INDEX]
+
+        console.log("data", requestInfo, method, pathRequest)
+
+        if (pathRequest === '/') {
+            socket.write(`${httpVersion} 200 OK\r\n\r\n`)
+        } else {
+            socket.write(`${httpVersion} 404 Not Found\r\n\r\n`)
+
+        }
+        socket.end();
+    })
+
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
