3a57b11e52c81c6c4dbd7fe71f1dc3c830677df7
Loi Nguyen <nguyenthanhloi.theone@gmail.com>
2024-06-01 13:09:37 +0700

    stage 4 - Respond with body

--- app/main.ts
+++ app/main.ts
@@ -4,11 +4,12 @@ const METHOD_INDEX = 0
 const PATH_REQUEST_INDEX = 1
 const HTTP_VERSION_INDEX = 2
 
+const PATH_ECHO = '/echo'
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const dataString = data.toString()
-        const dataArray = dataString.split('\r')
+        const dataArray = dataString.split('\r\n')
 
         const requestInfo = dataArray[0].split(' ')
 
@@ -16,10 +17,14 @@ const server = net.createServer((socket) => {
         const pathRequest = requestInfo[PATH_REQUEST_INDEX]
         const httpVersion = requestInfo[HTTP_VERSION_INDEX]
 
-        console.log("data", requestInfo, method, pathRequest)
+        console.log("data", dataArray,requestInfo, method, pathRequest)
 
         if (pathRequest === '/') {
             socket.write(`${httpVersion} 200 OK\r\n\r\n`)
+        } if(pathRequest.startsWith(PATH_ECHO)) {
+            const dataEcho = pathRequest.slice(PATH_ECHO.length + 1)
+
+            socket.write(`${httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dataEcho.length}\r\n\r\n${dataEcho}`)
         } else {
             socket.write(`${httpVersion} 404 Not Found\r\n\r\n`)
 
